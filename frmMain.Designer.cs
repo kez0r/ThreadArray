a2ad40e60bc0ced13f22c0b1e93936fc28ac0de0
Shawn <kez@jigmatic.com>
2016-09-07 02:30:01 -0400

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ThreadArray
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumThreads = new System.Windows.Forms.TextBox();
+            this.lblThreads = new System.Windows.Forms.Label();
+            this.lblOperations = new System.Windows.Forms.Label();
+            this.txtOperations = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.threadTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumThreads
+            // 
+            this.txtNumThreads.Location = new System.Drawing.Point(12, 12);
+            this.txtNumThreads.Name = "txtNumThreads";
+            this.txtNumThreads.Size = new System.Drawing.Size(49, 22);
+            this.txtNumThreads.TabIndex = 0;
+            this.txtNumThreads.Text = "10";
+            this.txtNumThreads.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblThreads
+            // 
+            this.lblThreads.AutoSize = true;
+            this.lblThreads.Location = new System.Drawing.Point(67, 15);
+            this.lblThreads.Name = "lblThreads";
+            this.lblThreads.Size = new System.Drawing.Size(46, 13);
+            this.lblThreads.TabIndex = 1;
+            this.lblThreads.Text = "threads";
+            // 
+            // lblOperations
+            // 
+            this.lblOperations.AutoSize = true;
+            this.lblOperations.Location = new System.Drawing.Point(174, 15);
+            this.lblOperations.Name = "lblOperations";
+            this.lblOperations.Size = new System.Drawing.Size(101, 13);
+            this.lblOperations.TabIndex = 3;
+            this.lblOperations.Text = "operations/thread";
+            // 
+            // txtOperations
+            // 
+            this.txtOperations.Location = new System.Drawing.Point(119, 12);
+            this.txtOperations.Name = "txtOperations";
+            this.txtOperations.Size = new System.Drawing.Size(49, 22);
+            this.txtOperations.TabIndex = 2;
+            this.txtOperations.Text = "50";
+            this.txtOperations.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // threadTimer
+            // 
+            this.threadTimer.Tick += new System.EventHandler(this.threadTimer_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(171, 40);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(101, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblOperations);
+            this.Controls.Add(this.txtOperations);
+            this.Controls.Add(this.lblThreads);
+            this.Controls.Add(this.txtNumThreads);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FrmMain";
+            this.Text = "Thread Array Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumThreads;
+        private System.Windows.Forms.Label lblThreads;
+        private System.Windows.Forms.Label lblOperations;
+        private System.Windows.Forms.TextBox txtOperations;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer threadTimer;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadArray
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        
+        public static Thread[] threadArray;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtNumThreads.Text.Trim() == "") return;
+
+            var numThreads = Convert.ToInt32(txtNumThreads.Text);
+            if (numThreads <= 0) return;
+
+            var operationsPer = Convert.ToInt32(txtOperations.Text);
+            if (operationsPer <= 0) return;
+
+            Console.WriteLine("Starting... ({0} thread/s, {1} operation/s per)", numThreads, operationsPer);
+            
+            //populate the checker thread array
+            threadArray = new Thread[numThreads];
+
+            for (var i = 0; i < threadArray.Count(); i++)
+            {
+                var t = i; //clone iteration variable
+
+                //assign SampleWorkerMethod to worker thread and start
+                threadArray[t] = new Thread(() => SampleWorkerMethod(threadArray[t].Name, operationsPer)) { Name = "Worker " + (t + 1) };
+                threadArray[t].Start();
+            }
+
+            //thread timer
+            threadTimer.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            foreach (var thread in threadArray)
+                thread.Abort();
+        }
+
+        public void SampleWorkerMethod(string threadName, int operationsPerThread)
+        {
+            //this is a simple sample method
+
+            for (var i = 0; i < operationsPerThread; i++)
+            {
+                var t = i; //clone iteration variable
+                Console.WriteLine("Thread [{0}]: Operation #{1}", threadName, (t + 1));
+            }
+        }
+
+        private void threadTimer_Tick(object sender, EventArgs e)
+        {
+            //check if any threads are still running
+            var runningThreads = threadArray.Any(thread => thread.IsAlive);
+
+            //thread(s) are still active
+            if (runningThreads) return;
+
+            //threads are finished running (or stopped), notify user of thread completion
+            Console.WriteLine("Thread operations completed!");
+
+            //turn off thread timer
+            threadTimer.Enabled = false;
+        }
+    }
+}
